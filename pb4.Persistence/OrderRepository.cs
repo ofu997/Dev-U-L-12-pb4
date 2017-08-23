98929883f7c9989d96a93558aa8c201b2c0c26b5
oliver fu <ofu997@gmail.com>
2017-08-23 13:53:27 -0700

    Add convertToEntity()

--- pb4.Persistence/OrderRepository.cs
+++ pb4.Persistence/OrderRepository.cs
@@ -11,23 +11,49 @@ namespace pb4.Persistence
 		public static void CreateOrder()
 		{
 			var db = new pb4DbEntities();
-			var order = new Order();
+
+			var order = ConvertToEntity();
+			/*
 			order.OrderId = Guid.NewGuid();
+
 			//order.Size= 1;
 			// this below works for now
 			order.Size = DTO.Enums.SizeType.Large;
-			// DTO.Enums.SizeType.Large
-			order.Crust= DTO.Enums.CrustType.Thick;
-			order.Pepperoni=true;
-			order.Name="the tech academy";
-			order.Address="300 SW 5th Avenue";
-			order.Zip="97201";
-			order.Phone="123-1234";
-			order.PaymentType=DTO.Enums.PaymentType.Credit;
-			order.TotalCost=16.50M;
 
+			// DTO.Enums.SizeType.Large
+			order.Crust = DTO.Enums.CrustType.Thick;
+			order.Pepperoni = true;
+			order.Name = "the tech academy";
+			order.Address = "300 SW 5th Avenue";
+			order.Zip = "97201";
+			order.Phone = "123-1234";
+			order.PaymentType = DTO.Enums.PaymentType.Credit;
+			order.TotalCost = 16.50M;
+			*/
 			db.Orders.Add(order);
 			db.SaveChanges();
+		}
+
+		private static Order ConvertToEntity()
+		{
+			var order = new Order();
+			order.OrderId = Guid.NewGuid();
+
+			//order.Size= 1;
+			// this below works for now
+			order.Size = DTO.Enums.SizeType.Large;
+
+			// DTO.Enums.SizeType.Large
+			order.Crust = DTO.Enums.CrustType.Thick;
+			order.Pepperoni = true;
+			order.Name = "the tech academy";
+			order.Address = "300 SW 5th Avenue";
+			order.Zip = "97201";
+			order.Phone = "123-1234";
+			order.PaymentType = DTO.Enums.PaymentType.Credit;
+			order.TotalCost = 16.50M;
+
+			return order;
 		}
     }
 }
