fc9322c28597f2dd91fceaabd723de88f90ef861
oliver fu <ofu997@gmail.com>
2017-08-22 19:45:49 -0700

    change Persistence class to OrderRepoitory, write class

---
+++ pb4.Persistence/OrderRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb4.Persistence
+{
+    public class OrderRepository
+    {
+		public static void CreateOrder()
+		{
+			var db = new pb4DbEntities();
+			var order = new Order();
+			order.OrderId = Guid.NewGuid();
+			order.Size=1;
+			order.Crust=0;
+			order.Pepperoni=true;
+			order.Name="the tech academy";
+			order.Address="300 SW 5th Avenue";
+			order.Zip="97201";
+			order.Phone="123-1234";
+			order.PaymentType=0;
+			order.TotalCost=16.50M;
+
+			db.Orders.Add(order);
+			db.SaveChanges();
+		}
+    }
+}
