70c2da6985b72addbd4fb57c6d57a37d340637af
oliver fu <ofu997@gmail.com>
2017-08-22 20:45:31 -0700

    Add DTO enums

---
+++ pb4.DTO/Enums/PaymentType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pb4.DTO.Enums
+{
+	public enum PaymentType
+	{
+		Cash, Credit 
+	}
+}
